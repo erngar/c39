4a7da75bd7d3067c8b9032378de1c8b1edd58af5
erngar <ernesto2583@gmail.com>
2023-10-11 08:39:49 -0400

    first try

---
+++ server.js
@@ -0,0 +1,53 @@
+const express = require('express')
+const app = express() //now app can use all methods in express
+const cors = require('cors')
+const PORT = 8000
+
+app.use(cors())
+
+//simple object to use for our json response
+const rappers  = {
+
+    '21 savage': {
+        'age': 29,
+        'birthName': 'Sheyaa',
+        'birthLocation': 'London, England'
+},
+
+'chance the rapper': {
+    'age': 29,
+    'birthName': 'Chacelor',
+    'birthLocation': 'Chicago, Illinois'
+},
+
+'unknown': {
+    'age': 0,
+    'birthName': 'unknown',
+    'birthLocation': 'unknown'
+}
+
+}
+
+//serve up a file to make sure all is working
+
+app.get('/', (request, response) =>{
+    response.sendFile(__dirname + '/index.html')
+})
+
+//another route
+app.get('/api/:name', (request,response) =>{
+    const rapperName = request.params.name.toLocaleLowerCase()
+    if(rappers[rapperName]){
+        response.json(rappers[rapperName])
+    }else{
+        response.json(rappers['unknown'])
+    }  
+   
+})
+
+//tell the server which port to listen from
+//use PORT variable instead of port #
+app.listen(process.env.PORT || PORT, ()=>{
+    console.log(`The server is now running on port ${PORT}`);
+})
+
